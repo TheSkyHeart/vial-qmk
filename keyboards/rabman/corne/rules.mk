73e06554c803b00a5940b0797b088266725db896
Rabi Hanna <rabi.android@gmail.com>
2024-09-04 22:48:53 +0000

    I think I figured it out

--- keyboards/rabman/corne/rules.mk
+++ keyboards/rabman/corne/rules.mk
@@ -1,16 +1,2 @@
 CONVERT_TO=rp2040_ce
 DEFAULT_FOLDER = rabman/corne/rev1
-VIA_ENABLE          = yes
-VIAL_ENABLE         = yes
-VIALRGB_ENABLE      = yes
-LTO_ENABLE          = yes
-
-RGBLIGHT_ENABLE     = no
-RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
-MOUSEKEY_ENABLE     = yes
-OLED_ENABLE         = yes
-#OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = yes
-COMBO_ENABLE        = yes
-
-QMK_SETTINGS        = yes
